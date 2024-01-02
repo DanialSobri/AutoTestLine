f152c326bd6479fb5a8b67f6cd0d4c578aa16951
Danial Sobri <73534490+DanialSobri@users.noreply.github.com>
2024-01-03 01:49:23 +0800

    Fix: Resolve programm stuck

--- app/main.py
+++ app/main.py
@@ -3,12 +3,17 @@ import time
 import pushrank
 
 def job_pushrank():
-    speed_test_instance = pushrank.SpeedTest()
-    speed_test_instance.run_test()
-    speed_test_instance.close()
+    try:
+        speed_test_instance = pushrank.SpeedTest()
+        speed_test_instance.run_test()
+        speed_test_instance.close()
+    except:
+        speed_test_instance = pushrank.SpeedTest()
+        speed_test_instance.run_test()
+        speed_test_instance.close()
 
-# schedule.every(10).seconds.do(job)
-schedule.every(30).minutes.do(job_pushrank)
+# schedule.every(60).seconds.do(job_pushrank)
+schedule.every(20).minutes.do(job_pushrank)
 # schedule.every().day.at("10:30").do(job)
 
 while 1:
